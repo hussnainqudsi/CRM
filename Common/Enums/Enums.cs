16bea7e19b66a94795fb569126f48dfc2acc6dbd
M. Hussnain Qudsi <86054556+hussnainqudsi@users.noreply.github.com>
2021-12-01 16:21:04 +0500

    Add files via upload
    Common

---
+++ Common/Enums/Enums.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Enums
+{
+    public class Enums
+    {
+        public enum Category
+        {
+            Apartment = 1,
+            Commercial =2,
+            Residential=3,
+            Villas=4  
+        }
+        public enum Source
+        {
+            facebook=1,
+            Instagram=2,
+            Website=3
+        }
+        public enum Status
+        {
+            Pending=1,
+            Completed=2,
+            Canceled=3,
+            In_Progress = 4
+        }
+    }
+}
