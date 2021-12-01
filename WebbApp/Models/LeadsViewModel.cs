f00e54208f9b3fdc9fa0824fae58ff75f84f3031
M. Hussnain Qudsi <86054556+hussnainqudsi@users.noreply.github.com>
2021-12-01 16:29:44 +0500

    Add files via upload

---
+++ WebbApp/Models/LeadsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Service.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebbApp.Models
+{
+    public class LeadsViewModel
+    {
+        public List<LeadsDto> ListLeads { get; set; }
+        public LeadsDto Leads { get; set; }
+        public List<SocietyDto> ListSociety { get; set; }
+        public DescriptionDto Description { get; set; }
+        public List<DescriptionDto> ListDescription { get; set; }
+    }
+}
---
+++ WebbApp/Models/SocietyViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Service.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebbApp.Models
+{
+    public class SocietyViewModel
+    {
+        public List<SocietyDto> ListSociety { get; set; }
+        public SocietyDto Society { get; set; }
+    }
+}
