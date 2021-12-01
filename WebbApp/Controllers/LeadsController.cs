357a180e59f9c2e6f9a23324ada8b0e1889b1a0c
M. Hussnain Qudsi <86054556+hussnainqudsi@users.noreply.github.com>
2021-12-01 16:28:51 +0500

    Add files via upload

---
+++ WebbApp/Controllers/LeadsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service.Interfaces;
+using System;
+using System.Threading.Tasks;
+using WebbApp.Models;
+
+namespace WebbApp.Controllers
+{
+    public class LeadsController : Controller
+    {
+        private ILeadsInterface _leadsInterface;
+        private IDescriptionInterface _descriptionInterface;
+        private ISocietyInterface _societyInterface;
+        public LeadsController(ILeadsInterface _leadsInterface, ISocietyInterface _societyInterface, IDescriptionInterface _descriptionInterface)
+        {
+            this._leadsInterface = _leadsInterface;
+            this._societyInterface = _societyInterface;
+            this._descriptionInterface = _descriptionInterface;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var vm = new LeadsViewModel();
+            vm.ListLeads = await _leadsInterface.GetAll();
+            return View(vm);
+        }
+        public async Task<IActionResult> AddLead()
+        {
+            var vm = new LeadsViewModel();
+            vm.ListSociety = await _societyInterface.GetAll();
+            return View(vm);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddLead(LeadsViewModel leadsViewModel)
+        {
+            await _leadsInterface.Add(leadsViewModel.Leads);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebbApp/Controllers/SocietyController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service.Implementations;
+using Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebbApp.Models;
+
+namespace WebbApp.Controllers
+{
+    public class SocietyController : Controller
+    {
+        private ISocietyInterface _societyInterface;
+        public SocietyController(ISocietyInterface _societyInterface)
+        {
+            this._societyInterface = _societyInterface;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var vm = new SocietyViewModel();
+            vm.ListSociety = await _societyInterface.GetAll();
+            return View(vm);
+        }
+        public IActionResult AddSociety()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddSociety(SocietyViewModel societyViewModel)
+        {
+            var data = await _societyInterface.Add(societyViewModel.Society);
+            return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> UpdateSociety(int id)
+        {
+            var vm = new SocietyViewModel();
+            vm.Society = await _societyInterface.Get(id);
+            return View(vm);
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateSociety(SocietyViewModel societyViewModel)
+        {
+            await _societyInterface.Update(societyViewModel.Society);
+            return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> DeleteSociety(int id)
+        {
+            await _societyInterface.Delete(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
